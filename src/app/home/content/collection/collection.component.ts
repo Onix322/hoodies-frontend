f77f9dbee10c8f89e0a685077d6d33ee44f9d5be
Alex <131547906+Onix322@users.noreply.github.com>
2025-01-07 14:38:05 +0100

    Frontend Hoodie

---
+++ src/app/home/content/collection/collection.component.ts
@@ -0,0 +1,50 @@
+import {AfterViewInit, Component, Input} from '@angular/core';
+import {SeeProductsButtonComponent} from '../../../utils/buttons/see-products-button/see-products-button.component';
+import {ProductBoxComponent} from '../../../product-box/product-box.component';
+
+@Component({
+  selector: 'app-collection',
+  imports: [
+    SeeProductsButtonComponent,
+    ProductBoxComponent,
+  ],
+  templateUrl: './collection.component.html',
+  standalone: true,
+  styleUrl: './collection.component.css'
+})
+export class CollectionComponent implements AfterViewInit {
+
+  @Input() title: string = "";
+  @Input() textStyle: string = "";
+  @Input() containerStyle: string = "";
+  @Input() articlesStyle: string = "";
+  words: Array<String> = [];
+
+
+  ngAfterViewInit(): void {
+    this.words = this.transformStringToArrayOfWords();
+  }
+
+  public transformStringToArrayOfWords() {
+
+    // (& -> is a character for adding a space on the same line
+
+    const arrayWords: Array<String> = [];
+    let word: String = "";
+
+    for (let i = 0, j = 0; i < this.title.length; i++) {
+
+      if (this.title.charAt(i) === " " || i === this.title.length - 1) {
+        arrayWords[j] = word;
+        j++;
+        word = "";
+        arrayWords.push(word);
+        continue
+      }
+
+      word += this.title.charAt(i);
+    }
+
+    return arrayWords.map(w => w.replaceAll("(&", " "));
+  }
+}
