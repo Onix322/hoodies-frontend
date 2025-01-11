80f1c4991e733223945206f9b76d36c74ee5916d
Alex <131547906+Onix322@users.noreply.github.com>
2025-01-11 22:44:08 +0100

    DELETED files from product-crud directory and created product-crud.component with crud operations
    DELETED files from user-crud directory for adding later user-crud.component
    
    CHANGES in product crud si dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,15 +1,6 @@
 import {Component, ComponentRef, createComponent, ElementRef, ViewChild, ViewContainerRef} from '@angular/core';
 import {ProfileButtonComponent} from '../../utils/buttons/profile-button/profile-button.component';
-import {CreateProductComponent} from '../product-crud/create-product/create-product.component';
-import {DeleteProductComponent} from '../product-crud/delete-product/delete-product.component';
-import {GetProductComponent} from '../product-crud/get-product/get-product.component';
-import {EditProductComponent} from '../product-crud/edit-product/edit-product.component';
-import {GetAllProductsComponent} from '../product-crud/get-all-products/get-all-products.component';
-import {CreateUserComponent} from '../user-crud/create-user/create-user.component';
-import {DeleteUserComponent} from '../user-crud/delete-user/delete-user.component';
-import {GetUserComponent} from '../user-crud/get-user/get-user.component';
-import {GetAllUsersComponent} from '../user-crud/get-all-users/get-all-users.component';
-import {EditUserComponent} from '../user-crud/edit-user/edit-user.component';
+import {ProductCrudComponent} from '../product-crud/product-crud.component';
 import {NgIf} from '@angular/common';
 
 @Component({
@@ -22,16 +13,8 @@ import {NgIf} from '@angular/common';
 export class DashboardComponent {
 
   @ViewChild("content", {read: ViewContainerRef}) content: ViewContainerRef | undefined;
-  protected readonly CreateUserComponent = CreateUserComponent;
-  protected readonly CreateProductComponent = CreateProductComponent;
-  protected readonly GetProductComponent = GetProductComponent;
-  protected readonly GetAllUsersComponent = GetAllUsersComponent;
-  protected readonly GetAllProductsComponent = GetAllProductsComponent;
-  protected readonly DeleteProductComponent = DeleteProductComponent;
-  protected readonly EditProductComponent = EditProductComponent;
-  protected readonly GetUserComponent = GetUserComponent;
-  protected readonly DeleteUserComponent = DeleteUserComponent;
-  protected readonly EditUserComponent = EditUserComponent;
+  protected readonly CreateProductComponent = ProductCrudComponent;
+
   public activeView: boolean = false;
 
   public renderCrudContent(component: any) {
@@ -40,4 +23,6 @@ export class DashboardComponent {
     this.content.clear()
     this.content.createComponent(component)
   }
+
+  protected readonly ProductCrudComponent = ProductCrudComponent;
 }
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,5 +1,7 @@
-import {booleanAttribute, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {firstValueFrom, Observable} from 'rxjs';
+import {ResponseWrapper} from '../../utils/response/response-wrapper';
 
 @Injectable({
   providedIn: 'root'
@@ -8,22 +10,27 @@ import {HttpClient} from '@angular/common/http';
 export class ProductService {
 
   private url: string = "http://localhost:8080/products/"
-  constructor(private http: HttpClient) { }
 
-  public post(body: any){
+  constructor(private http: HttpClient) {
 
+  }
+
+  public post(body: any) {
     this.http.post(this.url + "post", body).subscribe()
   }
 
-  public delete(id: Number){
+  public delete(id: Number) {
     this.http.delete(this.url + "delete/" + id).subscribe()
   }
 
-  public get(id: Number){
-    this.http.get(this.url + "get/" + id).subscribe()
+  public async get(id: Number): Promise<ResponseWrapper>{
+    return await firstValueFrom(this.http.get<ResponseWrapper>(this.url + "get/" + id))
   }
 
-  public edit(body: any){
-    this.http.put(this.url + "put", body).subscribe(body)
+  public edit(body: any) {
+    this.http.put(this.url + "put", body).subscribe()
+    console.log(body)
   }
+
 }
+
