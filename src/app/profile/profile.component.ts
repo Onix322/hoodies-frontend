dde461c9e53703b01ab95af86180a9f0b3d36dfb
Alex <131547906+Onix322@users.noreply.github.com>
2025-02-02 18:53:03 +0100

    Added edit infos functionality in profile page so the user can change his infos.
    Added dashboard button if the role is ADMIN for easier acces to Dashboard

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,33 +1,46 @@
-import {Component, OnInit, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Redirect} from '../utils/redirect/redirect';
 import {AuthService} from '../services/auth/auth.service';
 import {UserService} from '../services/user/user.service';
 import {NavComponent} from '../nav/nav.component';
 import {ChangePasswordComponent} from '../utils/popup/change-password/change-password.component';
+import {PopupComponent} from '../utils/popup/popup.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Notification} from '../utils/notifications/notification/notification';
+import {FormValidator} from '../utils/form-validator/form-validator';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   imports: [
     NavComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
+    PopupComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
   ],
   standalone: true,
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
-  @ViewChild(ChangePasswordComponent, {read: ChangePasswordComponent})
-  private popup: ChangePasswordComponent | undefined;
+  @ViewChild("changePasswordC", {read: ChangePasswordComponent})
+  private changePasswordPopup: ChangePasswordComponent | undefined;
 
-  @Output() id: number = 0;
-  @Output() name: string = "";
-  @Output() email: string = "";
-  @Output() phone: string = "";
-  @Output() role: string = "";
-  @Output() userImage: string = "";
+  @ViewChild("changeInfos", {read: PopupComponent})
+  private changeInfosPopup: PopupComponent | undefined;
 
-  constructor(private authService: AuthService, private userService: UserService, private redirect: Redirect) {
+  @Input() id: number = 0;
+  @Input() name: string = "";
+  @Input() email: string = "";
+  @Input() phone: string = "";
+  @Input() role: string = "";
+  @Input() userImage: string = "";
+  @Input() activationStatus: string = "";
+
+  constructor(private authService: AuthService, private userService: UserService, private redirect: Redirect, private validator: FormValidator) {
     this.redirect.toIfNotAuth("/login")
   }
 
@@ -43,6 +56,7 @@ export class ProfileComponent implements OnInit {
         this.phone = value.result.phone
         this.role = value.result.role
         this.userImage = value.result.userImage
+        this.activationStatus = value.result.activationStatus
       },
       error: err => {
         console.log(err)
@@ -50,12 +64,66 @@ export class ProfileComponent implements OnInit {
     })
   }
 
+  ngAfterViewInit() {
+    if (!this.validator.validate("edit-user")) return
+  }
+
   public logOut() {
     this.authService.logout()
     window.location.replace("/")
   }
 
-  public changePassword(){
-    this.popup?.open()
+  public openChangePassword(){
+    this.changePasswordPopup?.open()
+  }
+
+  public openChangeInfos(){
+    this.changeInfosPopup?.open()
+  }
+
+  public editUser() {
+
+    if (this.id < 1) {
+      Notification.notifyInvalid("Id must be greater than 0")
+      return
+    }
+
+    if (!this.validator.validate("edit-user")) return
+
+    const user = {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+      phone: this.phone,
+      role: this.role,
+      userImage: this.userImage,
+      activationStatus: this.activationStatus
+    }
+
+    this.userService.updateUser(user).subscribe({
+      next: () => {
+        Notification.notifyValid("User has been updated!")
+        this.changeInfosPopup?.close()
+      },
+      error: () => {
+        Notification.notifyInvalid("Something went wrong!")
+      }
+    })
+  }
+
+  public deleteAccount() {
+    this.userService.deactivate(this.id).subscribe({
+      next: () => {
+        Notification.notifyValid("User has been deactivated!")
+        this.logOut()
+      },
+      error: () => {
+        Notification.notifyInvalid("Something went wrong!")
+      }
+    })
+  }
+
+  public toDashboard(){
+    this.redirect.toIfAuth("/dashboard")
   }
 }
