07307f5a450de380f9b36068074585dfce177034
Alex <alexandru.dobos3@gmail.com>
2025-03-12 09:51:44 +0100

    added filters in Products page
    --WORKING ON FILTERS FUNCTIONALITY --

--- src/app/all-products-page/all-products-page.component.ts
+++ src/app/all-products-page/all-products-page.component.ts
@@ -4,11 +4,12 @@ import {ProductBoxComponent} from '../utils/product-box/product-box.component';
 import {FooterComponent} from '../utils/footer/footer.component';
 import {ProductService} from '../services/product/product.service';
 import {BehaviorSubject, delay, first} from 'rxjs';
+import {ProductStarRatingComponent} from '../utils/product-box/product-star-rating/product-star-rating.component';
 
 @Component({
   selector: 'app-all-products-page',
   standalone: true,
-  imports: [NavComponent, ProductBoxComponent, FooterComponent],
+  imports: [NavComponent, ProductBoxComponent, FooterComponent, ProductStarRatingComponent],
   templateUrl: './all-products-page.component.html',
   styleUrl: './all-products-page.component.css'
 })
